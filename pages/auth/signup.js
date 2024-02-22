e3de84494f41d57d9b29109b68e7970fd9d86831
Shazaniyu55 <shazaniyu@gmail.com>
2024-02-22 21:44:11 +0100

    niyu

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -1,22 +1,23 @@
 import React, { useRef, useState } from "react";
 import { styles } from "../../components/styles";
 import Link from "next/link";
-import  {useRouter} from 'next/router'
+import { useRouter } from 'next/router';
 
-
-const SignUp = ()=>{
+const SignUp = () => {
   const [formData, setFormData] = useState({});
   const [errorMessage, setErrorMessage] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
-  const navigate = useRouter()
-  //const [loading, setLoading] = useState(false);
+  const navigate = useRouter();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
 
-
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -36,7 +37,7 @@ const SignUp = ()=>{
         return setErrorMessage(data.message);
       }
       setLoading(false);
-      if(res.ok) {
+      if (res.ok) {
         navigate.push('/auth/login');
       }
     } catch (error) {
@@ -45,27 +46,25 @@ const SignUp = ()=>{
     }
   };
 
-    return(
-        <div className="flex justify-center items-center m-20">
-            <div className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
-            <p className={styles.sectionSubText}>Bitnex Fx</p>
-            <h3 className={styles.sectionHeadText}>SigUp.</h3>
-            <form
+  return (
+    <div className="flex justify-center items-center m-20">
+      <div className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
+        <p className={styles.sectionSubText}>Bitnex Fx</p>
+        <h3 className={styles.sectionHeadText}>SignUp.</h3>
+        <form
           onSubmit={handleSubmit}
           className='mt-12 flex flex-col gap-8'
         >
-           <label className='flex flex-col'>
+          <label className='flex flex-col'>
             <span className='text-white font-medium mb-4'>Upload photo</span>
             <input
               type='file'
               name='file'
-              
               onChange={handleChange}
-              placeholder="What's your good name?"
+              placeholder="Upload photo"
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
-
 
           <label className='flex flex-col'>
             <span className='text-white font-medium mb-4'>Full Name</span>
@@ -83,57 +82,85 @@ const SignUp = ()=>{
               type='email'
               id='email'
               onChange={handleChange}
-              placeholder="What's your web address?"
+              placeholder="What's your email address?"
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
           <label className='flex flex-col'>
             <span className='text-white font-medium mb-4'>Password</span>
-            <input
-              type='password'
-              id='password'
-              onChange={handleChange}
-              placeholder="Password"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
-          </label>
-
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Confirm Password</span>
-            <input
-              type='password'
-              id='cpassword'
-              onChange={handleChange}
-              placeholder="confirm password"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
+            <div className="relative">
+              <input
+                type={showPassword ? 'text' : 'password'}
+                id='password'
+                onChange={handleChange}
+                placeholder="Password"
+                className='bg-tertiary py-4 pl-14 pr-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              />
+              <span
+                className="absolute right-4 top-1/2 transform-translate-y-1/2 cursor-pointer"
+                onClick={togglePasswordVisibility}
+              >
+                {showPassword ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 text-white"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 text-white"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M17 21a2 2 0 01-2 2H9a2 2 0 01-2-2v-7m4-2v-4a2 2 0 114 0v4m-4 0v4a2 2 0 104 0v-4"
+                    />
+                  </svg>
+                )}
+              </span>
+            </div>
           </label>
 
           <label className='flex flex-col'>
             <span className='text-white font-medium mb-4'>Already have an account? <Link href="/auth/login">Login</Link></span>
-
-            </label>
-
-         
+          </label>
 
           <button
             type='submit'
             className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
           >
-             {loading ? (
-                <>
-                  
-                  <span className='pl-3'>Loading...</span>
-                </>
-              ) : (
-                'Sign Up'
-              )}
+            {loading ? (
+              <>
+                <span className='pl-3'>Loading...</span>
+              </>
+            ) : (
+              'Sign Up'
+            )}
           </button>
         </form>
+      </div>
+    </div>
+  );
+};
 
-            </div>
-        </div>
-    )
-}
-
-export default SignUp
+export default SignUp;
